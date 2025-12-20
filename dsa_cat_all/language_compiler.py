5be982ffb4458e8233eb62706fee460472bf71d0
dsa_cat <113278747+dsacat@users.noreply.github.com>
2025-12-20 16:40:28 +0300

    Update language_compiler.py

--- dsa_cat_all/language_compiler.py
+++ dsa_cat_all/language_compiler.py
@@ -11,3 +11,6 @@ def language_compile():
     with open(BASE_DIR / "language", "r") as f:
         m = f.read()
     return (language, m)
+def change_language(language):
+    with open(BASE_DIR / "language", "w+") as f:
+        f.write(language)
